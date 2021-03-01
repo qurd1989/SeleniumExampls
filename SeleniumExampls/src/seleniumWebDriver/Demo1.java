aa7201c5d7be48bcf3f3ac80d40b4758a221110c
sahib <sahib@LAPTOP-GGFQJMI8.home>
2021-03-01 10:22:54 -0500

    attempt 2

--- SeleniumExampls/src/seleniumWebDriver/Demo1.java
+++ SeleniumExampls/src/seleniumWebDriver/Demo1.java
@@ -3,7 +3,8 @@ package seleniumWebDriver;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Murad bomba oglandi");
 
 	}
 
