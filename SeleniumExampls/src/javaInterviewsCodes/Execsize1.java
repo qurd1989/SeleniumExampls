bbe76ff723f2a69284ce741c17938680de68d22f
sahib <sahib@LAPTOP-GGFQJMI8.lan>
2021-03-09 08:29:29 -0500

    exercises

--- SeleniumExampls/src/javaInterviewsCodes/Execsize1.java
+++ SeleniumExampls/src/javaInterviewsCodes/Execsize1.java
@@ -3,8 +3,11 @@ package javaInterviewsCodes;
 public class Execsize1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		//1.Java Program to Reverse a Number
+		//2.Java Program to Check Whether a Number is Even or Odd
+		//3.Reverse String
+		System.out.println("*************");
 	}
 
 }
