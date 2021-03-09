60f606d0cc056fcaba9bcbeec48abf39eeb4881b
sahib <sahib@LAPTOP-GGFQJMI8.lan>
2021-03-09 08:50:24 -0500

    changes

---
+++ SeleniumExampls/src/javaInterviewsCodes/EvenOdd.java
@@ -0,0 +1,27 @@
+package javaInterviewsCodes;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		//2.Java Program to Check Whether a Number is Even or Odd
+	    
+	     
+		 Scanner scan =new Scanner(System.in);
+	     System.out.println("Enter a number");
+	     int n=scan.nextInt();
+	     
+	     if(n%2==0) {
+	    	 System.out.println(n+" is Even");
+	     }
+	     else {
+	    	 System.out.println(n+" is Odd");
+	     }
+		
+		
+
+	}
+
+}
--- SeleniumExampls/src/javaInterviewsCodes/Execsize1.java
+++ SeleniumExampls/src/javaInterviewsCodes/Execsize1.java
@@ -1,11 +1,41 @@
 package javaInterviewsCodes;
 
+import java.util.Scanner;
+
 public class Execsize1 {
 
 	public static void main(String[] args) {
 	
 		//1.Java Program to Reverse a Number
+		
+//		int num=12345;
+//		int rev=0;
+//		
+//		while(num!=0) {
+//			rev=rev*10+num%10;
+//			num=num/10;
+//		}
+//    System.out.println("Reverse is:" +rev);
+		
+		
+		
+		
 		//2.Java Program to Check Whether a Number is Even or Odd
+    
+     
+	 Scanner scan =new Scanner(System.in);
+     System.out.println("Enter a number");
+     int n=scan.nextInt();
+     
+     if(n%2==0) {
+    	 System.out.println(n+" is Even");
+     }
+     else {
+    	 System.out.println(n+" is Odd");
+     }
+    
+    
+    
 		//3.Reverse String
 		System.out.println("*************");
 	}
